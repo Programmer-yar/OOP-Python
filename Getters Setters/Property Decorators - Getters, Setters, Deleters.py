9c735489f367d09d599d2d37262e44c073b36c17
yar <ahmadyar228@gmail.com>
2022-01-22 13:16:28 +0500

    Setters & Deleters

--- Getters Setters/Property Decorators - Getters, Setters, Deleters.py
+++ Getters Setters/Property Decorators - Getters, Setters, Deleters.py
@@ -18,6 +18,12 @@ class Employee:
         self.first = first
         self.last = last
 
+    @fullname.deleter
+    def fullname(self):
+        print('fullname deleted!')
+        self.first = None
+        self.last = None
+
 
 emp_1 = Employee('John', 'Smith')
 
@@ -32,7 +38,8 @@ and call this method like 'instance.email()'
 but this approach is not desireable
 so we will use 'property' decorator
 - property decorator
-allows us to access method like an attribute
+allows us to access method like an attribute without the need for other
+people(already using this class) to change their code
 """
 
 print(emp_1.first)
@@ -48,6 +55,16 @@ then we will get (can't set attribute) error, which means that we need define Se
 
 # Try this by commenting out fullname setter
 emp_1.fullname = 'Corey Schafer'
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname, '\n')
+
+"""
+- Deleters:
+if we want to delete an attribute we can set a deleter method for 
+that attribute
+"""
+del emp_1.fullname
 print(emp_1.first)
 print(emp_1.email)
 print(emp_1.fullname)
