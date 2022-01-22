961f404404b4e4ebb12ae21918569a8ceb9efaab
yar <ahmadyar228@gmail.com>
2022-01-22 13:07:08 +0500

    property decorator

---
+++ Getters Setters/Property Decorators - Getters, Setters, Deleters.py
@@ -0,0 +1,53 @@
+class Employee:
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+        # self.email = f'{first}.{last}@gmail.com'
+
+    @property
+    def email(self):
+        return f'{self.first}.{self.last}@gmail.com'
+
+    @property
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
+    @fullname.setter
+    def fullname(self, name):
+        first, last = name.split(' ')
+        self.first = first
+        self.last = last
+
+
+emp_1 = Employee('John', 'Smith')
+
+# changing the 'first' Name of employee after instance creation
+emp_1.first = 'Jim'
+
+"""
+Email will still contain 'John', 
+Now we will remove 'self.email' from constructor '__init__'
+and make class method called 'email()' 
+and call this method like 'instance.email()'
+but this approach is not desireable
+so we will use 'property' decorator
+- property decorator
+allows us to access method like an attribute
+"""
+
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname, '\n')
+
+"""
+- Setters:
+if we want to change the fullname like
+'instance.fullname = "Corey Schafer"'
+then we will get (can't set attribute) error, which means that we need define Setter
+"""
+
+# Try this by commenting out fullname setter
+emp_1.fullname = 'Corey Schafer'
+print(emp_1.first)
+print(emp_1.email)
+print(emp_1.fullname)
